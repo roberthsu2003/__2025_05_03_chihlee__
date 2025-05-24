fcc301220b761e248db6ee4dde672d2e9bd89304
roberthsu2003 <roberthsu2003@gmail.com>
2025-05-24 14:32:52 +0800

    modify

--- lesson8/lesson8_1.py
+++ lesson8/lesson8_1.py
@@ -1,13 +1,16 @@
-import tools
+#import edu
+#from edu.tools import caculate_bmi,get_state
+from edu.tools import caculate_bmi as a1
+from edu.tools import get_state as a2
 
 def main():
     height:int = int(input("請輸入身高(cm):"))
     weight:int = int(input("請輸入體重(kg):"))
 
-    bmi = tools.caculate_bmi(height, weight)
+    bmi = a1(height, weight)
 
     print(bmi)
-    print(tools.get_state(bmi))
+    print(a2(bmi))
 
 
 if __name__ == '__main__':
